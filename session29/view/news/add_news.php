b8bb31d07af89dc67d3f589eb71521dc3a9dd929
= <=>
2019-07-13 19:24:40 +0700

    bta

---
+++ session29/view/news/add_news.php
@@ -0,0 +1,13 @@
+<h1>Add news Page </h1>
+<form action="index.php?action=add_product" method="post">
+	<p>Title
+		<input type="text" name="tilte">
+	</p>
+	<p>Describe
+		<textarea name="describe" rows="8"></textarea>
+	</p>
+	<p>Date Submitted
+		<input type="text" name="datesubmit">
+	</p>
+	<p><input type="submit" name="add_news_form" value="Add product"></p>
+</form>
--- session29/view/news/news.php
+++ session29/view/news/news.php
@@ -1,6 +1,37 @@
 <h1>News page here</h1>
+<a href="index.php?action=add_news">Add News</a>
 <?php echo $showNews;?>
-
+<table class="table table-bordered">
+  <tr>
+    <th style="width: 10px">#</th>
+    <th>Title</th>
+    <th>Describe</th>
+    <th>Date Submitted</th>
+    <th>Image</th>
+  </tr>
+ <?php 
+ if ($productList->num_rows > 0) {
+ 	while($row = $newsList->fetch_assoc()) {
+ 		$id = $row['id'];
+ ?>
+    <tr>
+      <td><?php echo $row['id']?></td>
+      <td><?php echo $row['title']?></td>
+      <td>
+        <?php echo $row['describe']?>
+      </td>
+      <td><img src="uploads/<?php echo $row['image']?>" alt="image" class="avatar_user"></td>
+       <td><?php echo $row['created']?></td>
+      <td><a href="index.php?action=edit_news&id=<?php echo $id?>">Edit</a> | <a href="index.php?action=delete_edit&id=<?php echo $id?>">Delete</a></td>
+    </tr>
+  <?php 
+  	}
+  } else {?>
+  <tr>
+  	<td colspan="4">Khong co tin tuc nao</td>
+  </tr>
+  <?php }?>
+</table>
 
 <div id="news-more">
 <?php echo $related;?>
